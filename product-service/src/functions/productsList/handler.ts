7c0385d0abb48e5c8c9cadf4e924fa76cc2e86be
Mike <mikeshakmakov@gmail.com>
2021-07-16 18:34:33 +0300

    task3 - cors fix

--- product-service/src/functions/productsList/handler.ts
+++ product-service/src/functions/productsList/handler.ts
@@ -1,8 +1,13 @@
+import { formatJSONError } from './../../libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import * as json from './adidas.json';
 
 export const main = async () => {
-  return await Promise.resolve(formatJSONResponse({
-    products: json.data
-  }));
+  try {
+    return await Promise.resolve(formatJSONResponse({
+      products: json.data
+    }));
+  } catch (e) {
+    return formatJSONError(500, 'something went wrong');
+  }
 };
--- product-service/src/libs/apiGateway.ts
+++ product-service/src/libs/apiGateway.ts
@@ -1,6 +1,10 @@
 export const formatJSONResponse = (response: Record<string, unknown>) => {
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+  },
     body: JSON.stringify(response)
   }
 }
